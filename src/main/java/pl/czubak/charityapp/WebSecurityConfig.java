ff2f2f3364fffdbce60064766a86055842f6bc8d
Jakub Czubak <czubakjakub94@gmail.com>
2020-01-25 12:24:52 +0100

    add MySimpleUrlAuthenticationSuccessHandler

--- src/main/java/pl/czubak/charityapp/WebSecurityConfig.java
+++ src/main/java/pl/czubak/charityapp/WebSecurityConfig.java
@@ -4,17 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Bean("authenticationManager")
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -32,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordParameter("password")
                 .usernameParameter("email")
                 .defaultSuccessUrl("/donation")
+                .successHandler(myAuthenticationSuccessHandler())
                 .and().logout().logoutSuccessUrl("/")
                 .and()
                 .httpBasic();
@@ -39,6 +46,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new MySimpleUrlAuthenticationSuccessHandler();
+    }
 
     @Bean(name = "dataSource")
     public DriverManagerDataSource dataSource() {
